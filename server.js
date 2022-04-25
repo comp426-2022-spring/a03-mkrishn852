239b67bef4879a8845d58a6204c2665433096713
Maya Krishnamoorthy <mkrishn@email.unc.edu>
2022-04-25 18:31:31 -0400

    default

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get('/app/flip/call/tails', (req, res) => {
 
 // Default response for any other request
 app.use(function(req, res){
-  res.status(404).send('404 NOT FOUND')
+  res.status(404).send('404 NOT FOUND') 
 });
 
 
